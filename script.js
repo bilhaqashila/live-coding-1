ae83e38480a79d7fc1351d62dd28ccadc061385c
bilhaqashila <bilhaqashila@gmail.com>
2025-09-02 19:46:54 +0700

    5 menit

---
+++ script.js
@@ -0,0 +1,20 @@
+//reverse and times array
+// input
+// [1,4,6,7]
+// [7,4,2,1]
+// [5]
+
+// output
+// [14,12,8,2]
+// [2,4,8,14]
+// [10]
+
+function revntime (array) {
+    let result = []
+    for (let i = array.length ; i = 0 ; i--) {
+        a = array[i] - 1
+        multiply = a * 2
+        result.push(multiply)
+    }
+    console.log()
+}
