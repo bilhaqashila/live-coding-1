c3cb3bbfb356ab2e8c0a49323dfa97e9e34c6ba5
bilhaqashila <bilhaqashila@gmail.com>
2025-09-02 20:22:04 +0700

    40 menit

--- script.js
+++ script.js
@@ -11,13 +11,16 @@
 
 function revntimes (array) {
     const result = []
-    for (let i = array.length ; i < 0 ; i--) {
-        let a = array[i] - 1
+    for (let i = array.length - 1 ; i >= 0 ; i--) {
+        let a = array[i] 
         let multiply = a * 2
-        result.push(multiply) 
+        result.push(multiply)  
     }
     console.log(result) 
 } 
  
 const array1 = revntimes([1,4,6,7])
-console.log(array1)    
+
+const array2 = revntimes([7,4,2,1])
+
+const array3 = revntimes([5])
